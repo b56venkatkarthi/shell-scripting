fd1f6a55950734dd7b001575d2b42aa9de5c4e7d
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2023-12-25 18:09:54 -0500

    “Autopush

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -1,11 +1,9 @@
 #!/bin/shell
 
-
 echo welcome to shell scripting
 echo "This is day 1 of our shell scripting"
 
 
-echo line1
-echo line1
-echo line1
-echo line5
+echo "Line1\nLine2"
+echo line3
+echo line4
