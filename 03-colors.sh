f96a96862e0a11046187a1f573c938e7ce0bfcac
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2023-12-26 18:21:00 -0500

    “Autopush

--- 03-colors.sh
+++ 03-colors.sh
@@ -5,3 +5,5 @@ echo -e "\e[32m I am printing green color \e[0m"
 echo -e "\e[33m I am printing yellow color \e[0m"
 echo -e "\e[34m I am printing red blue \e[0m"
 echo -e "\e[35m I am printing red magneta \e[0m"
+
+echo -e "\e[43;34m I am printing background + foreground \e[m"
