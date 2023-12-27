3302d0d47839ffd8517359096f1441e2940ebc77
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2023-12-26 21:42:37 -0500

    “Autopush

---
+++ 04-variabales.sh
@@ -0,0 +1,16 @@
+
+#what is a vaaibale ?
+# A variable is somethingg that holds data dynamically
+
+
+# the advantage & disadvanthage in shell is that there are no data type
+
+a=10
+b=abc
+
+echo $a
+echo printing the value of a $a
+echo printing the value of a ${a}
+
+
+echo printing the value of b ${b}
