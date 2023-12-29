63199a6818fc614a5f58be2142dda75dcac7b400
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2023-12-29 15:33:15 -0500

    “Autopush

---
+++ 10-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+#There are 4 tyes of commands
+
+#1.Binary 
+# 2. Alaiases
+#3. Built in comands
+#4 functions
+
+
+# how to declare a function
+
+# f () {
+#    echo hai
+#}
+
+#How to call a function
+ 
+# f calling a function
+
+b56(){
+    echo "This is our batch 56 function"
+    echo "we are leaning function"
+    echo "Today date is :"
+    date 
+    echo"function b56 is completed"
+}
