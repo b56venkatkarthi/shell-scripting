0a8acc095301084b0485821e13a776462bdae6c1
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2023-12-29 15:15:30 -0500

    “Autopush

---
+++ 08-quotes.sh
@@ -0,0 +1,9 @@
+
+
+echo "$$"  # $$ is going to print the PID of the current process
+echo '$$'  # single quotes alwasy eliminates the power of the special variable
+
+
+echo $?
+echo "$?"
+echo '$?'
