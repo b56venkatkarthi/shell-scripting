4c0bb7bafcb347f1e4aa2f86f2ce576c2f78c993
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2023-12-29 17:04:49 -0500

    “Autopush

---
+++ 11-comment.sh
@@ -0,0 +1,12 @@
+#!/bin/shell
+
+# below is the line for multi line comment
+
+<<comment
+echo "cloud Devops Training"
+echo "shell scripting"
+a=100
+b=100
+echo $a
+COMMENT
+echo "value of b is $b"
