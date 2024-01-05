720d3e6e79be3784439416f3d4d4c2d6ef51a264
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 11:55:29 -0500

    “Autopush

--- 13-condtions.sh
+++ 13-condtions.sh
@@ -10,22 +10,22 @@
 #esac
 
 
-ACTION=$1   # &1 refrs first command line argument
+    ACTION=$1   # &1 refrs first command line argument
 
 case ACTION in
     start)
-       ehco -e "\e [32 m statrting shipping service \e[0m"
+       ehco -e "\e [32m statrting shipping service \e[0m"
        exit 0
        ;;
     stop)
-        echo -e  "\e[32 m stoping new shipping service \e[0m"
+        echo -e  "\e[32m stoping new shipping service \e[0m"
         exit1
         ;;
     restart)
-        echo -e  "\e[33 m Restarting new shipping service \e[0m"
+        echo -e  "\e[33m Restarting new shipping service \e[0m"
         exit3
         ;;
     *)
-        echo -e  "\e[35 m valid options are start or stop or restart only \e[0m"
-        echo -e  "\e[33 m Example usage : \e[0m \n\t\t bash script.sh start"
+        echo -e  "\e[35m valid options are start or stop or restart only \e[0m"
+        echo -e  "\e[33m Example usage : \e[0m \n\t\t bash script.sh start"
 esac
