2d1410b8ce1bf7a3ba125eba8c40c614fe564574
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 12:41:04 -0500

    “Autopush

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -8,6 +8,12 @@ if [ "$ACTION" ==  start ] ; then
     echo -e "\e[32m starting shipping service \e [0m"
     exit 0
 
+else
+
+   echo -e "\e[33m valid option is start only \e [0m"
+    exit 1
+
+
 fi
 
  echo  "it has not met any contion"
