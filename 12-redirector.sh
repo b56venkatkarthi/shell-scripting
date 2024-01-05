509d6d2fed2bda4a1a426fd09bbe2c26c79563b5
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 11:42:55 -0500

    “Autopush

---
+++ 12-redirector.sh
@@ -0,0 +1,7 @@
+#!/bin/shell
+
+
+Redirectors are of two types in bash
+
+Input Redirector   ( <) we use to take input from file
+Output Rediector    (>> or >) means routing an output to file
---
+++ 13-condtions.sh
@@ -0,0 +1,34 @@
+#!/bin/shell
+
+# syntax of case
+
+#case $var in
+
+#opt1) command -x ;;
+#opt2) command -y ;;
+
+#esac
+
+
+ACTION=&1
+
+case
+
+start )
+ehco -e "\e [32 m statrting shipping service \e[0m"
+exit 0
+;;
+stop)
+
+echo -e  "\e[32 m stoping new shipping service \e[0m"
+exit1
+;;
+restart)
+echo -e  "\e[33 m Restarting new shipping service \e[0m"
+exit2
+;;
+*)
+echo -e  "\e[35 m valid options are start or stop or restart only \e[0m"
+echo -e  "\e[33 m Example usage : \e[0m bash script.sh start"
+
+esac
