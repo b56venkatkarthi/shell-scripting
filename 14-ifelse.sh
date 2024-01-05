f86dfbf3edb95578c7238f9a5e1bce13f0e8f01b
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 12:45:11 -0500

    “Autopush

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -5,13 +5,19 @@ echo -e "Demo on if ,if else & else if usage"
 ACTION=$1
 
 if [ "$ACTION" ==  start ] ; then
-    echo -e "\e[32m starting shipping service \e [0m"
+    echo -e "\e[32m starting shipping service \e[0m"
     exit 0
+elseif [ "$ACTION" ==  stop ] ; then  
+    echo -e "\e[33m stopping shipping service \e[0m"
+    exit 1
+    elseif [ "$ACTION" ==  restart ] ; then  
+    echo -e "\e[34m restart shipping service \e[0m"
+    exit 2
 
 else
 
-   echo -e "\e[33m valid option is start only \e [0m"
-    exit 1
+   echo -e "\e[31m valid option is start only \e[0m"
+    exit 3
 
 
 fi
