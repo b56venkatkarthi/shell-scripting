4bf3cbfd7d4f4cfcabd7f1f8e3895eed8e5e3bc1
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 15:44:33 -0500

    “Autopush

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -3,8 +3,8 @@
 
 bash components/$1.sh
 
-if [ $? <> 0 ] ; then
-    echo -e "\e[32m example usage : \e[0m bash wrapper.sh componenName"
-   exit 30
+#if [ $? <> 0 ] ; then
+ #   echo -e "\e[32m example usage : \e[0m bash wrapper.sh componenName"
+  # exit 30
 
-fi
+#fi
