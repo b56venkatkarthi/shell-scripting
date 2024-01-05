2473b4b114c58daa872c68a595f583da722d19ea
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 15:39:52 -0500

    “Autopush

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -4,7 +4,7 @@
 bash components/$1.sh
 
 if [ $? <> 0 ] ; then
-    echo -e "\e[32m starting shipping service \e[0m"
+    echo -e "\e[32m example usage : \e[0m bash wrapper.sh componenName"
    exit 30
 
 fi
