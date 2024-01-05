3999bd3152b1a925f0c822ec968213dbfc8a6c6f
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-05 12:30:37 -0500

    “Autopush

---
+++ 14-ifelse.sh
@@ -0,0 +1,11 @@
+#!/bin/shell
+
+echo -e "Demo on if ,if else & else if usage"
+
+ACTION = $1
+
+if [ $ACTION == start ] ; then
+    echo -e "\e[32m starting shipping service \e [0m"
+    exit 0
+
+fi
