9a3f29c76bff12746ea3a8e46632c88846727d1c
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-06 10:29:19 -0500

    “Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -20,6 +20,15 @@ if [ $? -eq 0 ] ; then
 else
   echo -e "\e[32m failure  \e[0m"
 fi
+
+echo -n "Downloading Component $1 :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+if [ $? -eq 0 ] ; then
+  echo -e "\e[31m success \e[0m"
+else
+  echo -e "\e[32m failure  \e[0m"
+fi
 # systemctl enable nginx
 # systemctl start nginx
 
