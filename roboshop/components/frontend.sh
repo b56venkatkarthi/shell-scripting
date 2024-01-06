309a784dbc24cb8b2a78fb20abe2909f5c808f92
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-06 16:12:20 -0500

    “Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-COMPONENT=$1
+COMPONENT=frontend
 LOGFILE=&>> "/tmp/${COMPONENT}.log"
 
 if [ $USER_ID -ne 0 ]; then 
--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-bash components/frontend.sh
+bash components/$1.sh
 
 #if [ $? <> 0 ] ; then
  #   echo -e "\e[32m example usage : \e[0m bash wrapper.sh componenName"
