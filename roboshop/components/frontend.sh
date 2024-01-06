0b4422a677805e382599fae79743f5ec77c3db33
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-06 17:15:43 -0500

    “Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,12 +5,19 @@ COMPONENT=frontend
 LOGFILE="/tmp/${COMPONENT}.log"
 
 stat() {
+if [ $? -eq 0 ] ; then
+  echo -e "\e[31m success \e[0m"
+else
+  echo -e "\e[32m failure  \e[0m"
+fi
+}
+
 if [ $USER_ID -ne 0 ]; then 
     echo -e "\e[31m this script is expected to be executed with sudo or root user\e[0m"
     echo -e "\e[35m Example usage: \n\t\t  \e[0m sudo bash scriptName componentName"
     exit 1
 fi
-}
+
     
 echo -e "****** \e[34m configure frontend  service \e[0m ******"
 echo -e  "Installing Nginx :"
---
+++ roboshop/components/mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+COMPONENT=frontend
+LOGFILE="/tmp/${COMPONENT}.log"
+
+stat() {
+if [ $USER_ID -ne 0 ]; then 
+    echo -e "\e[31m this script is expected to be executed with sudo or root user\e[0m"
+    echo -e "\e[35m Example usage: \n\t\t  \e[0m sudo bash scriptName componentName"
+    exit 1
+fi
+}
