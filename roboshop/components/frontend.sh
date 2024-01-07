80afe556a59591cd61c557756ccea54e21a6b152
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-07 12:04:19 -0500

    “Autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,7 +5,7 @@ COMPONENT="frontend"
 LOGFILE="/tmp/${COMPONENT}.log"
 
 stat() {
-if [ ${COMPONENT} -eq 0 ] ; then
+if [ $1 -eq 0 ] ; then
   echo -e "\e[31m success \e[0m"
 else
   echo -e "\e[32m failure  \e[0m"
