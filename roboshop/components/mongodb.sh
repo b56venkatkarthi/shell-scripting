7fd029df0d651fb928422f9b77c1296b486170b3
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-08 15:14:23 -0500

    “Autopush

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-COMPONENT=frontend
+COMPONENT=mongo
 LOGFILE="/tmp/${COMPONENT}.log"
 
 stat() {
@@ -11,3 +11,13 @@ if [ $USER_ID -ne 0 ]; then
     exit 1
 fi
 }
+
+
+if [ $USER_ID -ne 0 ]; then 
+    echo -e "\e[31m this script is expected to be executed with sudo or root user\e[0m"
+    echo -e "\e[35m Example usage: \n\t\t  \e[0m sudo bash scriptName componentName"
+    exit 1
+fi
+
+
+echo -e "******\e[35m  configuring of ${COMPONENT}  \e[0m ******"
