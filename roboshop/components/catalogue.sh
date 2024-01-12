3a4ccf09e84a533f9a89e092c5a527cc35067f30
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-12 17:30:39 -0500

    “Autopush

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+
+stat() {
+if [ $1 -eq 0 ] ; then
+  echo -e "\e[31m success \e[0m"
+else
+  echo -e "\e[32m failure  \e[0m"
+fi
+}
+
+
+if [ $USER_ID -ne 0 ]; then 
+    echo -e "\e[31m this script is expected to be executed with sudo or root user\e[0m"
+    echo -e "\e[35m Example usage: \n\t\t  \e[0m sudo bash scriptName componentName"
+    exit 1
+fi
+
+echo -n "Configuring NodeJs Repo ;"
+ecurl --silent --location https://rpm.nodesource.com/pub_16.x | sudo bash -
+stat$?
+
+echo -n "Installing NodeJs  ;"
+yum install nodejs -y  &>>  $LOGFILE
+stat$?
