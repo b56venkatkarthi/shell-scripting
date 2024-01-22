5720de35be952f75e0fc9f6530a04603fe7106ae
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-22 13:49:39 -0500

    “Autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -13,9 +13,12 @@ echo -e "******* \e[32m $COMPONENT \e[0mServer Creation in Progress]  ******!!!!
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]"
 PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
 
+echo -e "******* \e[32m $COMPONENT-$ENV \e[0m Server Creation Is Compelted ******* !!!!!! \n\n"
+
 
 echo -e "*******  \e[33m $COMPONENT \e[0m DNS Record Creation in Progress ******!!!!!"
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+#sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json.json
 
