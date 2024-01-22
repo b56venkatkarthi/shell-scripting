85fde4e1a81c4aa5dc49f6a43601ca6f7dfa266f
venkatkarthi <karthigapandiarasu@Karthigas-Air.lan>
2024-01-22 13:36:30 -0500

    “Autopush

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -6,9 +6,21 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all"| jq ".SecurityGroups[].GroupId" | sed -e 's/"//g') #never hardcode the ami
 INSTANCE_TYPE="t3.micro"
 COMPONENT=$1
+HOSTEDZONEID="Z074951730BXMKX1S7VS1"
 
-echo -e "*******8 server Creation in Progress ******!!!!!"
-
+echo -e "******* \e[32m $COMPONENT \e[0mServer Creation in Progress]  ******!!!!!"
 
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]"
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
+
+
+echo -e "*******  \e[33m $COMPONENT \e[0m DNS Record Creation in Progress ******!!!!!"
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/dns.json
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json.json
+
+
+
+
+
 
